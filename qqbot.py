f0ccb35eb02d4ab613833d0908a8ce7e797181c0
tr276913271 <276913271@qq.com>
2018-07-12 22:48:39 +0800

    roll

--- qqbot.py
+++ qqbot.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import json, requests
+import json, requests, random
 def onQQMessage(bot, contact, member, content):
   if content[:5:] != '[@ME]':
      return 0
@@ -10,6 +10,8 @@ def onQQMessage(bot, contact, member, content):
   elif content == '你该睡觉了':
     bot.SendTo(contact,'本萌妹睡了，不要喊我了')
     bot.Stop()
+  elif content.startswith('roll'):
+    bot.SendTo(contact,roll(content))
   else:
     d = {
       'key' : '',
@@ -26,3 +28,17 @@ def onQQMessage(bot, contact, member, content):
       bot.SendTo(contact,rs['text'] + '\n' + rs['url'])
     else:
       bot.SendTo(contact,'暂时不支持的技能，来个程序员啊！')
+
+def roll(content):
+  if(not content.startswith("roll")):
+    return 0
+  try:
+    strlist = content.split(" ")
+    result = 0
+    if(len(strlist)>1):
+      result = random.randint(0,int(strlist[1]))
+    else:
+      result = random.randint(0,100)
+  except:
+    result = random.randint(0,100)
+  return result
