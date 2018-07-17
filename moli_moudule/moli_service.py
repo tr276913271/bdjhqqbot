d3e0389eeda27b352dadbe9995f382c9af4fdd0f
smaxad47 <assassin.of.abyss@gmail.com>
2018-07-17 15:40:36 +0800

    茉莉模块fix

--- moli_moudule/moli_service.py
+++ moli_moudule/moli_service.py
@@ -10,4 +10,14 @@ class MoliService:
         }
         rs = requests.post('http://i.itpk.cn/api.php', data = moliDate)
         rs = rs.text
-        return rs
+        if content == '月老灵签':
+            if rs.startswith(u'\ufeff'):
+                rs = rs.encode('utf8')[3:].decode('utf8')
+                rs = json.loads(rs, encoding='utf8')
+                rs = rs['number2'] + " 卦象：" + rs['haohua'] + " 诗意：" + rs['shiyi'] + " 解签：" + rs['jieqian']
+            return rs
+        elif content == '笑话' or content == '灵签':
+            rs = '暂时不支持的功能哦~'
+            return rs
+        else:
+            return rs
