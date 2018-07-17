f8111eff8d1141888bfbae8e05de8d79ea86efdd
smaxad47 <assassin.of.abyss@gmail.com>
2018-07-17 16:55:08 +0800

    likemember模块及偷窃更新

---
+++ db_module/like_member.py
@@ -0,0 +1,23 @@
+#- * -coding: utf - 8 - * -
+from db_module.connect_db import DBHelper
+from db_module.data_tuple_to_str import DBTStr
+# 获取传入字符串中群员的名字，需将名字前面的字用strip割掉,数据库中不全的名字
+class LikeMember:
+    def cutMember(self, nameInContent):
+        member = nameInContent[:7]
+        return member
+
+    def likeMember(self, content):
+        memberName = self.cutMember(content) + '%'
+        memberName = DBHelper().selectOne(" select name from user where name like '"+memberName+"' ")
+        memberName = memberName.__str__()
+        return memberName
+
+    #查询群员是否在数据库里
+    def likeMemberBe(self,content):
+        memberName = self.cutMember(content) + '%'
+        if len(DBHelper().selectAll(" select name from user where name like '"+memberName+"' ")) == 0:
+            return False
+        else:
+            return True
+
