7e1a1d19a9c2023eef4499d96705096c2e9c5fb1
smaxad47 <assassin.of.abyss@gmail.com>
2018-08-01 19:44:37 +0800

    par

--- other_module/parents.py
+++ other_module/parents.py
@@ -19,6 +19,8 @@ class ParentService:
         contents = '我爸爸妈妈是七花和妹控哦~'
     elif (content.find('爹') >= 0):
         contents = '我爸爸是七花哦~'
+    elif (content.find('小姨') >= 0):
+        contents = '是零七小阿姨哦~她超可爱的~'
     else:
         contents = False
     return contents
